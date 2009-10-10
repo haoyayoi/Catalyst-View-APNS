2dadf4760ac7fa068887875c2fc78f54428e24ea
haoyayoi <st.hao.yayoi@gmail.com>
2009-10-10 11:45:53 +0900

    initial commit

---
+++ lib/Catalyst/View/APNS.pm
@@ -0,0 +1,33 @@
+package Catalyst::View::APNS;
+
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Catalyst::View::APNS -
+
+=head1 SYNOPSIS
+
+  use Catalyst::View::APNS;
+
+=head1 DESCRIPTION
+
+Catalyst::View::APNS is
+
+=head1 AUTHOR
+
+haoyayoi E<lt>st.hao.yayoi@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
